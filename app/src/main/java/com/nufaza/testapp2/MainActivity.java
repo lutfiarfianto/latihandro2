0d7eb375d8d76a76770b3f65dee3dc6b644c95f5
lutfi <subcoder@ditpsd.net>
2017-09-07 14:59:57 +0700

    Signed-off-by: lutfi <subcoder@ditpsd.net>

--- app/src/main/java/com/nufaza/testapp2/MainActivity.java
+++ app/src/main/java/com/nufaza/testapp2/MainActivity.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +30,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        final MaterialDialog md = new MaterialDialog.Builder(MainActivity.this)
+                .title("Info")
+                .content("Anda menulis " + ed.getText() + ". Bener? ")
+                .negativeText("Nggak Ah")
+                .positiveText("Iyah")
+                .show();
 
+        md.getActionButton(DialogAction.NEGATIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Oh nggak yah?", Toast.LENGTH_LONG).show();
+                md.dismiss();
+            }
+        });
+
+        md.getActionButton(DialogAction.POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Tuuuh bener kan?", Toast.LENGTH_LONG).show();
+                md.dismiss();
+            }
+        });
 
     }
 }
