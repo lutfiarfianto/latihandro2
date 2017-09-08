fd5bf19107867e11e22192dd06f53d2c293d60d7
lutfi <subcoder@ditpsd.net>
2017-09-08 08:37:41 +0700

    Signed-off-by: lutfi <subcoder@ditpsd.net>

--- app/src/main/java/com/nufaza/testapp2/MainActivity.java
+++ app/src/main/java/com/nufaza/testapp2/MainActivity.java
@@ -1,15 +1,23 @@
 package com.nufaza.testapp2;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         final TextView tv = (TextView) findViewById(R.id.textHasil);
         final EditText ed = (EditText) findViewById(R.id.textEditor);
         Button btn = (Button) findViewById(R.id.btnGanti);
+
+        // Ini temporary storage untuk listView.
+        List<String> listNama = new ArrayList<>(); // Harus diinisialisasi
+
+        // Ini listview diset sebagai class variable agar accessible anywhere
+        ListView listViewNama = null;  // Inisialisasi di onCreate
+
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,4 +70,41 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+    // Buat adapter yang mendefinisikan bahwa adapter ini disuplay datanya dari listNama
+    private class StrListAdapter extends ArrayAdapter<String> {
+        public StrListAdapter() {
+            super(MainActivity.this, R.layout.lv_item, listNama);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            // Pastikan ada view kalau null
+            View itemView = convertView;
+
+            if (itemView == null) {
+
+                // Cari itemview yang mendefinisikan format penampilan data di tiap baris listview
+                itemView = MainActivity.this.getLayoutInflater().inflate(R.layout.lv_item, parent, false);
+
+                // Untuk setiap baris, ambil dari listview nama
+                String namaStr = listNama.get(position);
+
+                // Cari textview di format tadi, terus kita isi dengan data.
+                TextView textViewNama = (TextView) itemView.findViewById(R.id.textViewNama);
+
+                // Kemudian isikan textview tsb dengan string nama
+                textViewNama.setText(namaStr);
+            }
+
+            return itemView;
+        }
+    }
+
+
 }
+
+
